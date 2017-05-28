a886ef5fe7e946fabcafade0a89e300425bb5ce4
Frederic Dib <frederic.dib@gmail.com>
2017-05-28 10:14:53 +0200

    fix double loaded of context

--- src/main/java/fdi/games/services/AppInitializer.java
+++ src/main/java/fdi/games/services/AppInitializer.java
@@ -5,7 +5,6 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
 import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
@@ -13,21 +12,12 @@ public class AppInitializer implements WebApplicationInitializer {
 
 	@Override
 	public void onStartup(ServletContext servletContext) throws ServletException {
-		final AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
-
-		// Déclaration de la classe de configuration
-		// du contexte (dataSource, repositories, spring-security …)
-		rootContext.register(AppConfig.class);
-		servletContext.addListener(new ContextLoaderListener(rootContext));
-
-		// Création du dispatcher de servlet
 		final AnnotationConfigWebApplicationContext dispatcherServlet = new AnnotationConfigWebApplicationContext();
 		dispatcherServlet.register(AppConfig.class);
 
-		// Déclaration du dispatcher de servlet
 		final ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcher",
 				new DispatcherServlet(dispatcherServlet));
 		dispatcher.setLoadOnStartup(1);
-		dispatcher.addMapping("/");
+		dispatcher.addMapping("/*");
 	}
 }
